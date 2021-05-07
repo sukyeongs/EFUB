5773e77d29e6e2a5343e7d103e9ee4700861f8ff
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-05-08 01:16:57 +0900

    Add files via upload

---
+++ HW#5/efub/src/test/java/ssk/efub/efubCRUDexample/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssk.efub.efubCRUDexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
